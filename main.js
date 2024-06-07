004bcaf560dbac66fadaeaea6af0fb08070d62be
Creator74 <dhvanigupta2007@gmail.com>
2024-06-07 10:22:34 +0530

    add deploy workflow

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const button = document.getElementById("joke-button");
 console.log(button)
+const apiUrl = 'https://v2.jokeapi.dev/joke/Programming';
 button.addEventListener("click",jokeUpdate)
 let count = 1;
 
-const apiUrl = 'https://v2.jokeapi.dev/joke/Programming';
 
-function jokeUpdate() { 
+/*function jokeUpdate() { 
   fetch(apiUrl)
   .then(response => {
     if (response.ok) {
@@ -33,4 +33,57 @@ function jokeUpdate() {
     console.error(error); // Example: Logging the error to the console
   });
 
+}*/
+
+async function jokeUpdate() 
+{
+  console.log('calling',apiUrl);
+  const endpoints = [apiUrl,apiUrl]
+  //const result = await fetch(apiUrl,{method: 'GET',});
+  const results = await Promise.all(endpoints.map(makeAPICall));
+  console.log(results);
+  //const data = await results.json()
+  //console.log(data)
+  if (results[0].type == "single")
+    {
+      let joke = results[0].joke
+      console.log(results[0].joke)
+      document.getElementById("demo1").innerHTML = "joke 1: " + joke;
+    }
+  else
+    {
+      let setup = results[0].setup
+      let delivery = results[0].delivery
+      document.getElementById("demo1").innerHTML = "joke 1: " + setup +"\n"+ delivery;
+    }
+
+    if (results[1].type == "single")
+      {
+        let joke = results[1].joke
+        console.log(results[1].joke)
+        document.getElementById("demo2").innerHTML = "joke 2: " + joke;
+      }
+    else
+      {
+        let setup = results[1].setup
+        let delivery = results[1].delivery
+        document.getElementById("demo2").innerHTML = "joke 2: " + setup +"\n"+ delivery;
+      }
 }
+
+async function makeAPICall(endpoint) {
+  const response = await fetch(endpoint);
+  const data = await response.json();
+  return data;
+}
+/*async function makeMultipleAPICalls(endpoints) {
+  try{
+    const promises = endpoints.map(makeAPICall);
+  const responses = await Promise.all(promises);
+  return responses;
+  }
+  catch (error) 
+{
+  console.error('Error:', error);
+}
+}*/
